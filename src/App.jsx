c62036d9a5e2a9237ed374f55b2258a791813647
Ed Cabiltes <edcabiltes@outlook.com>
2025-01-18 15:29:23 +0800

    Added edit functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { useState } from 'react'
 import CustomForm from './components/CustomForm';
 import TaskList from './components/TaskList';
+import EditForm from './components/EditForm';
 
 function App() {
   const [tasks, setTasks] = useState([]);
+  const [previousFocusE1, setPreviousFocusE1] = useState(null);
+  const [editedTask, setEditedTask] = useState(null);
+  const [isEditing, setIsEditing] = useState(false);
 
   const addTask = (task) => {
     setTasks(prevState => [...prevState, task])
@@ -21,17 +25,47 @@ function App() {
     )))
   }
 
+  const updateTask = (task) => {
+    setTasks(prevState => prevState.map(t => (
+      t.id === task.id 
+        ? { ...t, name: task.name} 
+        : t
+    )))
+    closeEditMode();
+  }
+
+  const closeEditMode = () => {
+    setIsEditing(false);
+    previousFocusE1.focus();
+  }
+
+  const enterEditMode = (task) => {
+    setEditedTask(task);
+    setIsEditing(true);
+    setPreviousFocusE1(document.activeElement);
+  }
+
   return (
     <div className='container'>
       <header>
         <h1>My Task List</h1>
       </header>
+      {
+        isEditing && (
+          <EditForm
+            editedTask={editedTask}
+            updateTask={updateTask}
+            closeEditMode={closeEditMode}
+          />
+        )
+      }
       <CustomForm addTask={addTask} />
       {tasks && (
         <TaskList 
           tasks={tasks}
           deleteTask={deleteTask}
           toggleTask={toggleTask}
+          enterEditMode={enterEditMode}
         />
       )}
     </div>
